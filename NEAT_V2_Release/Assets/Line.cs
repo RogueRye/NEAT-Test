0dfe66499552e3cc7f3ffdb03178282f91a2716f
Ryan Hallisey <rhallis1@student.fitchburgstate.edu>
2018-11-28 20:23:50 -0500

    Initial commit

---
+++ NEAT_V2_Release/Assets/Line.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+
+    Transform point1;
+    Transform point2;
+    LineRenderer lineR;
+
+	// Use this for initialization
+	void Start () {
+        point1 = transform.Find("Point1");
+        point2 = transform.Find("Point2");
+        lineR = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        lineR.SetPosition(0, point1.position);
+        lineR.SetPosition(1, point2.position);
+	}
+}
---
+++ NEAT_V2_Release/Assets/Point.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour {
+
+
+    SpriteRenderer myRenderer;
+	// Use this for initialization
+	void Start () {
+        myRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        myRenderer.color = Color.red;
+    }
+
+    private void OnMouseDrag()
+    {
+        var mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorld.z = transform.position.z;
+
+        transform.position = mouseWorld;
+
+    }
+
+    private void OnMouseExit()
+    {
+        myRenderer.color = Color.white;
+    }
+}
